d9d0a579457e0b21e9699e64cd8ff53ec7f02c84
Yaruozo <yazuoru@gmail.com>
2025-11-14 07:46:34 +0800

    Fixed bug close button in delete

--- resources/views/livewire/pages/sales-management/promo.blade.php
+++ resources/views/livewire/pages/sales-management/promo.blade.php
@@ -482,7 +482,7 @@
                 <div class="fixed top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full flex items-center justify-center">
                     <div class="relative w-full max-w-md max-h-full">
                         <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                            <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" wire:click="cancel">
+                            <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" wire:click="cancelDelete">
                                 <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
                                 </svg>
@@ -496,7 +496,7 @@
                                 <flux:button wire:click="delete" class="mr-2 bg-red-600 hover:bg-red-700 text-white">
                                     Yes, I'm sure
                                 </flux:button>
-                                <flux:button wire:click="cancel" variant="outline">
+                                <flux:button wire:click="cancelDelete" variant="outline">
                                     No, cancel
                                 </flux:button>
                             </div>
